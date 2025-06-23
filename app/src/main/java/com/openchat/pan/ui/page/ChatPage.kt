880c27305f3a9c144973759b9dba03617829f644
Pan <pangao775@qq.com>
2025-06-23 19:58:16 +0800

    feat:页面调整

--- app/src/main/java/com/openchat/pan/ui/page/ChatPage.kt
+++ app/src/main/java/com/openchat/pan/ui/page/ChatPage.kt
@@ -1,12 +1,9 @@
 package com.openchat.pan.ui.page
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
