9607244687b9c639455008b7df3cb08c1848824b
Pan <pangao775@qq.com>
2025-06-23 19:57:51 +0800

    feat:页面调整

--- app/src/main/java/com/openchat/pan/ui/page/ChatPage.kt
+++ app/src/main/java/com/openchat/pan/ui/page/ChatPage.kt
@@ -1,15 +1,38 @@
 package com.openchat.pan.ui.page
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview
 @Composable
 fun ChatPage() {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text("聊天页面", style = MaterialTheme.typography.headlineMedium)
+    // 可以调用 ConversationListPage 来展示会话列表
+    ConversationListPage()
+}
+
+
+// conversation 列表
+@Composable
+fun ConversationListPage() {
+    // 列表
+    // 这里可以使用 LazyColumn 或者其他列表组件来展示会话列表
+    // 目前先简单展示一个文本
+    LazyColumn {
+        item {
+            Text("聊天页面", style = MaterialTheme.typography.headlineMedium)
+        }
+        item {
+            Text("会话列表", style = MaterialTheme.typography.bodyLarge)
+        }
+        // 这里可以添加更多的会话项
+        items(10) { index ->
+            Text("会话 $index", style = MaterialTheme.typography.bodyMedium)
+        }
     }
 }
